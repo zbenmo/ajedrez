a0bf838f7903a4efe615fa4c77e4c896e45de303
Oren <zbenmo@gmail.com>
2024-11-21 16:36:34 +0100

    Good point - still missing functionality (castleling, en-passant, checking for check-mate, creating the new board)..
    Probably more..

---
+++ ajedrez.py
@@ -0,0 +1,277 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Dict, Generator, Protocol, Tuple, Callable
+from collections import Counter
+import numpy as np
+
+
+Square = Tuple[int, int]
+GetSquareStatus = Callable[[Square], str]
+
+
+class Piece(ABC):
+    def __init__(self, location: Square, piece: str):
+        self.location = location
+        self.piece = piece
+
+    @abstractmethod
+    def theoretical_moves(
+            self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
+        pass
+
+
+class Pawn(Piece):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        direction = +1 if piece == 'p' else -1
+        starting_row = 1 if piece == 'p' else 6
+        row, col = location
+        self.moves = [(row + direction, col)]
+        if row == starting_row:
+            self.moves.append((row + 2 * direction, col))
+        self.captures = []
+        if col > 0:
+            self.captures.append((row + direction, col - 1))
+        if col < 7:
+            self.captures.append((row + direction, col + 1))
+
+    def theoretical_moves(
+            self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
+        for move in self.moves:
+            if get_square_status(move) == ' ':
+                yield move
+        for capture in self.captures:
+            if get_square_status(move).islower() != self.piece.islower():
+                yield capture
+
+
+class RayBased(Piece, ABC):
+    def __init__(self, location, piece):
+        super().__init__(location, piece)
+
+    def theoretical_moves(
+            self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
+        for ray in self.rays:
+            for move in ray:
+                status = get_square_status(move)
+                if status == ' ':
+                    yield move
+                else:
+                    if status.islower() != self.piece.islower():
+                        yield move # capture
+                    break # this ray reached a piece
+
+
+class Queen(RayBased):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        row, col = location
+        self.rays = [
+            [(row, c) for c in range(col + 1, 8)],
+            [(row, c) for c in range(col - 1, -1, -1)],
+            [(r, col) for r in range(row + 1, 8)],
+            [(r, col) for r in range(row - 1, -1, -1)],
+            [(r, c) for r, c in zip(range(row + 1, 8), range(col + 1, 8))],
+            [(r, c) for r, c in zip(range(row - 1, -1, -1), range(col + 1, 8))],
+            [(r, c) for r, c in zip(range(row + 1, 8), range(col - 1, -1, -1))],
+            [(r, c) for r, c in zip(range(row - 1, -1, -1), range(col - 1, -1, -1))],
+        ]
+
+
+class Rock(RayBased):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        row, col = location
+        self.rays = [
+            [(row, c) for c in range(col + 1, 8)],
+            [(row, c) for c in range(col - 1, -1, -1)],
+            [(r, col) for r in range(row + 1, 8)],
+            [(r, col) for r in range(row - 1, -1, -1)],
+        ]
+
+
+class Bishop(RayBased):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        row, col = location
+        self.rays = [
+            [(r, c) for r, c in zip(range(row + 1, 8), range(col + 1, 8))],
+            [(r, c) for r, c in zip(range(row - 1, -1, -1), range(col + 1, 8))],
+            [(r, c) for r, c in zip(range(row + 1, 8), range(col - 1, -1, -1))],
+            [(r, c) for r, c in zip(range(row - 1, -1, -1), range(col - 1, -1, -1))],
+        ]
+
+
+class Knight(Piece):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        row, col = location
+        self.moves = []
+        for add_row in [1, 2]:
+            add_col = 3 - add_row
+            for sign_row in [-1, 1]:
+                r = row + sign_row * add_row
+                if r > 7 or r < 0:
+                    continue
+                for sign_col in [-1, 1]:
+                    c = col + sign_col * add_col
+                    if c > 7 or c < 0:
+                        continue
+                    self.moves.append((r, c))
+
+    def theoretical_moves(
+            self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
+        for move in self.moves:
+            status = get_square_status(move) 
+            if (
+                status == ' ' # free
+                or status.islower() != self.piece.islower() # capture
+            ):
+                yield move
+
+
+class King(Piece):
+    def __init__(self, location: Square, piece: str):
+        super().__init__(location, piece)
+        row, col = location
+        self.moves = []
+        for add_row in [-1, 0, 1]:
+            r = row + add_row
+            if r < 0 or r > 7:
+                continue
+            for add_col in [-1, 0, 1]:
+                if add_row == 0 and add_col == 0:
+                    continue
+                c = col + add_col
+                if c < 0 or c > 7:
+                    continue
+                self.moves.append((r, c))
+
+    def theoretical_moves(
+            self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
+        for move in self.moves:
+            status = get_square_status(move) 
+            if (
+                status == ' ' # free
+                or status.islower() != self.piece.islower() # capture
+            ):
+                yield move
+
+
+class Game:
+    """Representing a current state or a Chess game.
+    A lot was copied from Chessnut, yet I wanted to speed up exploration and evaluation.
+    """
+    pass
+
+
+@dataclass
+class Board:
+    position: str # first part of fen
+    turn: str # should be either 'w' or 'b'
+    casteling: str # options still available
+    en_passant: str
+    half_moves: int
+    move_number: int
+    stats: Counter # how many of each piece are there. There might be some extra stuff there
+    piece_placement: 'np.array'
+    location_to_piece: Dict[Square, Piece]
+
+    def simple_heuristic(self) -> Tuple[float, float]:
+        vals = {
+            'p': 1.0,
+            'n': 3.0,
+            'b': 3.0,
+            'r': 5.0,
+            'q': 8.0,
+        }
+        white, black = 0., 0.
+        for piece, count in self.stats.items():
+            if piece.islower():
+                white += vals.get(piece, 0) * count
+            else:
+                black += vals.get(piece.lower(), 0) * count
+        return white, black
+
+
+default_fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+
+class Game:
+    def __init__(self, fen: str = default_fen):
+        (
+            position,
+            turn,
+            casteling,
+            en_passant,
+            half_moves,
+            move_number
+        ) = fen.split(' ')
+
+        def split_and_expand(line: str) -> Generator[str, None, None]:
+            for char in line:
+                if char.isdigit():
+                    for _ in range(int(char)):
+                        yield ' '
+                else:
+                    yield char
+
+        piece_placement = np.array([
+            list(split_and_expand(line))
+            for line in position.split('/')
+        ])
+
+        def piece_for(row: int, col: int):
+            piece: str = piece_placement[row, col]
+            match piece:
+                case 'p' | 'P':
+                    return Pawn((row, col), piece)
+                case 'r' | 'R':
+                    return Rock((row, col), piece)
+                case 'n' | 'N':
+                    return Knight((row, col), piece)
+                case 'b' | 'B':
+                    return Bishop((row, col), piece)
+                case 'q' | 'Q':
+                    return Queen((row, col), piece)
+                case 'k' | 'K':
+                    return Queen((row, col), piece)
+
+        self.board = Board(
+            position=position,
+            turn=turn,
+            casteling=casteling,
+            en_passant=en_passant,
+            half_moves=int(half_moves),
+            move_number=int(move_number),
+            stats=Counter(position),
+            piece_placement=piece_placement,
+            location_to_piece={
+                (row, col): piece_for(row, col)
+                for row in range(8)
+                for col in range(8)
+                if piece_placement[row, col] != ' '
+            }
+        )
+
+    def available_moves(self) -> Generator[Tuple[str, Game], None, None]:
+        for move in self._raw_moves():
+            yield f'{move}', self # TODO: TEMP
+
+    def _raw_moves(self) -> Generator[Tuple[int, int, int, int, str], None, None]:
+        for r, c, p in self.__current_player_pieces():
+            piece = self.board.location_to_piece[r, c]
+            for dest in piece.theoretical_moves(lambda square: self.board.piece_placement[square]):
+                yield r, c, *dest, p
+
+    def __current_player_pieces(self) -> Generator[Tuple[int, int, str], None, None]:
+        relevant_pieces = (
+            ['p', 'r', 'n', 'b', 'q', 'k']
+            if self.board.turn == 'w'
+            else ['P', 'R', 'N', 'B', 'Q', 'K']
+        )
+        for r, c in zip(*np.where(np.isin(self.board.piece_placement, relevant_pieces))):
+            yield r, c, self.board.piece_placement[r, c]
+
+    def __repr__(self):
+        return f"""{self.board}"""
