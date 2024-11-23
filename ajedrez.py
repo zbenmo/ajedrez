301ee9898b1ac648892ceec7870819c54526ee27
Oren <zbenmo@gmail.com>
2024-11-23 15:33:20 +0100

    probably got upcase vs. lowcase other way around

--- ajedrez.py
+++ ajedrez.py
@@ -9,6 +9,10 @@ import numpy as np
 Square = Tuple[int, int]
 GetSquareStatus = Callable[[Square], str]
 
+W_P, W_R, W_N, W_B, W_Q, W_K = "PRNBQK"
+B_P, B_R, B_N, B_B, B_Q, B_K = "prnbqk"
+EMPTY = " "
+
 
 class Piece(ABC):
     """"""
@@ -29,8 +33,8 @@ class Pawn(Piece):
     """"""
     def __init__(self, location: Square, piece: str):
         super().__init__(location, piece)
-        direction = +1 if piece == 'p' else -1
-        starting_row = 1 if piece == 'p' else 6
+        direction = +1 if piece == W_P else -1
+        starting_row = 1 if piece == W_P else 6
         row, col = location
         self.moves = [(row + direction, col)]
         if row == starting_row:
@@ -44,11 +48,11 @@ class Pawn(Piece):
     def theoretical_moves(
             self, get_square_status: GetSquareStatus) -> Generator[Square, None, None]:
         for move in self.moves:
-            if get_square_status(move) == ' ':
+            if get_square_status(move) == EMPTY:
                 yield move
         for capture in self.captures:
             status = get_square_status(move) 
-            if status != ' ' and get_square_status(move).islower() != self.piece.islower():
+            if status != EMPTY and get_square_status(move).islower() != self.piece.islower():
                 yield capture
 
 
@@ -62,7 +66,7 @@ class RayBased(Piece, ABC):
         for ray in self.rays:
             for move in ray:
                 status = get_square_status(move)
-                if status == ' ':
+                if status == EMPTY:
                     yield move
                 else:
                     if status.islower() != self.piece.islower():
@@ -136,7 +140,7 @@ class Knight(Piece):
         for move in self.moves:
             status = get_square_status(move) 
             if (
-                status == ' ' # free
+                status == EMPTY
                 or status.islower() != self.piece.islower() # capture
             ):
                 yield move
@@ -165,7 +169,7 @@ class King(Piece):
         for move in self.moves:
             status = get_square_status(move) 
             if (
-                status == ' ' # free
+                status == EMPTY
                 or status.islower() != self.piece.islower() # capture
             ):
                 yield move
@@ -200,9 +204,9 @@ class Board:
         white, black = 0., 0.
         for piece, count in self.stats.items():
             if piece.islower():
-                white += vals.get(piece, 0) * count
+                black += vals.get(piece, 0) * count
             else:
-                black += vals.get(piece.lower(), 0) * count
+                white += vals.get(piece.lower(), 0) * count
         return white, black
 
 
@@ -246,13 +250,13 @@ class Game:
             for char in line:
                 if char.isdigit():
                     for _ in range(int(char)):
-                        yield ' '
+                        yield EMPTY
                 else:
                     yield char
 
         piece_placement = np.array([
             list(split_and_expand(line))
-            for line in position.split('/')
+            for line in reversed(position.split('/'))
         ])
 
         return cls(
@@ -267,7 +271,7 @@ class Game:
                 (row, col): cls.piece_for(piece_placement[row, col], row, col)
                 for row in range(8)
                 for col in range(8)
-                if piece_placement[row, col] != ' '
+                if piece_placement[row, col] != EMPTY
             }
         )
 
@@ -299,36 +303,38 @@ class Game:
 
             row_from, col_from, row_to, col_to, piece, promotion = move
             status_dest = self.board.piece_placement[row_to, col_to]
-            if status_dest != ' ':
+            if status_dest != EMPTY:
                 stats[status_dest] -= 1
             location_to_piece[row_to, col_to] = (
                 Game.piece_for(promotion or piece, row_to, col_to) # potentially override
             )
             del location_to_piece[row_from, col_from] # the piece is already in its new place
-            piece_placement[row_from, col_from] = ' '
+            piece_placement[row_from, col_from] = EMPTY
             piece_placement[row_to, col_to] = promotion or piece
             if promotion:
                 stats[piece] -= 1
                 stats[promotion] += 1
 
             # en-passant (capture)
-            if (status_dest == ' ') and (piece == 'p' or piece == 'P') and (col_from != col_to):
-                target_row = row_to + 1 if piece == 'p' else row_to - 1
+            if (status_dest == EMPTY) and (piece == W_P or piece == B_P) and (col_from != col_to):
+                target_row = row_to + 1 if piece == W_P else row_to - 1
                 status_dest = self.board.piece_placement[target_row, col_to]
-                assert status_dest == 'p' or status_dest == 'P'
+                assert status_dest == W_P or status_dest == B_P
                 stats[status_dest] -= 1
                 del location_to_piece[target_row, col_to]
 
             # casteling
-            if (piece == 'k' or piece == 'K') and (abs(col_from - col_to) == 2):
+            is_casteling = False
+            if (piece == W_K or piece == B_K) and (abs(col_from - col_to) == 2):
+                is_casteling = True
                 if col_to == 6:
                     piece_placement[row_to, 5] = piece.board.piece_placement[row_to, 7]
-                    piece_placement[row_to, 7] = ' '
+                    piece_placement[row_to, 7] = EMPTY
                     del location_to_piece[row_to, 7]
                     location_to_piece[row_to, 5] = Game.piece_for(piece_placement[row_to, 5], row_to, 5)
                 elif col_to == 2:
                     piece_placement[row_to, 3] = piece.board.piece_placement[row_to, 0]
-                    piece_placement[row_to, 0] = ' '
+                    piece_placement[row_to, 0] = EMPTY
                     del location_to_piece[row_to, 0]
                     location_to_piece[row_to, 3] = Game.piece_for(piece_placement[row_to, 3], row_to, 3)
                 else:
@@ -343,22 +349,24 @@ class Game:
                 en_passant = chr(ord('a') + col_from)
 
             casteling = self.board.casteling # for next turn
-            if piece == 'k':
-                casteling = casteling.replace('k', '')
-                casteling = casteling.replace('q', '')
-            elif piece == 'K':
+            if piece == W_K:
                 casteling = casteling.replace('K', '')
                 casteling = casteling.replace('Q', '')
-            elif piece == 'r':
-                if col_from == 0:
-                    casteling = casteling.replace('q', '')
-                elif col_from == 7:
-                    casteling = casteling.replace('k', '')
-            elif piece == 'R':
+            elif piece == B_K:
+                casteling = casteling.replace('k', '')
+                casteling = casteling.replace('q', '')
+            elif piece == W_R:
                 if col_from == 0:
                     casteling = casteling.replace('Q', '')
                 elif col_from == 7:
                     casteling = casteling.replace('K', '')
+            elif piece == B_R:
+                if col_from == 0:
+                    casteling = casteling.replace('q', '')
+                elif col_from == 7:
+                    casteling = casteling.replace('k', '')
+            if len(casteling) < 1:
+                casteling = '-'
 
             g = Game(
                 other,
@@ -373,6 +381,7 @@ class Game:
 
             # is it a valid game/board?
             if g._is_check(which_king=self.board.turn):
+                assert not is_casteling # because I believe I've already verified there
                 # we either did not protect the king, or have exposed it
                 continue # it is not, that move should not be considered
 
@@ -383,7 +392,7 @@ class Game:
 
     def _is_check(self, which_king: str) -> bool:
         """Returns wheater the relevant king is being treated."""
-        king = "k" if which_king == "w" else "K"
+        king = W_K if which_king == "w" else B_K
         other = "b" if which_king == "w" else "w"
         king_location = np.where(self.board.piece_placement == king)
         return self._is_treatened_by(king_location, by=other)
@@ -408,22 +417,22 @@ class Game:
                 # above isinstance(piece, Pawn) is redundant yet added for clarity
                 en_passant = ord(self.board.en_passant) - ord('a')
                 if c != en_passant: # if it is exactly in the current column, we don't want to "add" it
-                    if p == 'p':
+                    if p == W_P:
                         magic_square = (5, en_passant)
-                        magic_piece = 'P'
-                    elif p == 'P':
+                        magic_piece = B_P
+                    elif p == B_P:
                         magic_square = (2, en_passant)
-                        magic_piece = 'p'
+                        magic_piece = W_P
 
             for dest in piece.theoretical_moves(
                 lambda square: magic_piece if square == magic_square else self.board.piece_placement[square]
                 ):
                 # offering options for promotion is done here, if relevant
-                if p == 'p' and dest[0] == 7:
-                    for promotion in ['q', 'r', 'b', 'n']:
+                if p == W_P and dest[0] == 7:
+                    for promotion in [W_Q, W_R, W_B, W_N]:
                         yield r, c, *dest, p, promotion
-                elif p == 'P' and dest[0] == 0:
-                    for promotion in ['Q', 'R', 'B', 'N']:
+                elif p == B_P and dest[0] == 0:
+                    for promotion in [B_Q, B_R, B_B, B_N]:
                         yield r, c, *dest, p, promotion
                 else:
                     yield r, c, *dest, p, None
@@ -433,17 +442,17 @@ class Game:
         if player == "w":
             other = "b"
             row = 0
-            king_casteling = "k"
-            queen_casteling = "q"
-            king = "k"
-            rook = "r"
+            king_casteling = "K"
+            queen_casteling = "Q"
+            king = W_K
+            rook = W_R
         else:
             other = "w"
             row = 7
-            king_casteling = "K"
-            queen_casteling = "Q"
-            king = "K"
-            rook = "R"
+            king_casteling = "k"
+            queen_casteling = "q"
+            king = B_K
+            rook = B_R
 
         if (king_casteling not in self.board.casteling) and (queen_casteling not in self.board.casteling):
             return
@@ -455,21 +464,21 @@ class Game:
 
         if king_casteling in self.board.casteling:
             assert self.board.piece_placement[row, 7] == rook
-            if self.board.piece_placement[row, 5] == ' ' and self.board.piece_placement[row, 6] == ' ':
+            if self.board.piece_placement[row, 5] == EMPTY and self.board.piece_placement[row, 6] == EMPTY:
                 if not self._is_treatened_by((row, 5), other) and not self._is_treatened_by((row, 6), other):
                     yield row, 4, row, 6, king, None
 
         if queen_casteling in self.board.casteling:
             assert self.board.piece_placement[row, 0] == rook
-            if self.board.piece_placement[row, 3] == ' ' and self.board.piece_placement[row, 2] == ' ':
+            if self.board.piece_placement[row, 3] == EMPTY and self.board.piece_placement[row, 2] == EMPTY:
                 if not self._is_treatened_by((row, 3), other) and not self._is_treatened_by((row, 2), other):
                     yield row, 4, row, 2, king, None
 
     def __current_player_pieces(self, player: str) -> Generator[Tuple[int, int, str], None, None]:
         relevant_pieces = (
-            ['p', 'r', 'n', 'b', 'q', 'k']
+            [W_P, W_R, W_N, W_B, W_Q, W_K]
             if player == 'w'
-            else ['P', 'R', 'N', 'B', 'Q', 'K']
+            else [B_P, B_R, B_N, B_B, B_Q, B_K]
         )
         for r, c in zip(*np.where(np.isin(self.board.piece_placement, relevant_pieces))):
             yield r.item(), c.item(), self.board.piece_placement[r, c].item()
